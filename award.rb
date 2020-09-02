d91636fc91f01e3f3a1d91743055497ffc36b5e2
Marnen Laibow-Koser <marnen@marnen.org>
2020-09-02 16:35:56 -0400

    Move update logic to an instance method.

--- award.rb
+++ award.rb
@@ -1,1 +1,47 @@
-Award = Struct.new(:name, :expires_in, :quality)
+Award = Struct.new(:name, :expires_in, :quality) do
+  def update_quality!
+    if self.name != 'Blue First' && self.name != 'Blue Compare'
+      if self.quality > 0
+        if self.name != 'Blue Distinction Plus'
+          self.quality -= 1
+        end
+      end
+    else
+      if self.quality < 50
+        self.quality += 1
+        if self.name == 'Blue Compare'
+          if self.expires_in < 11
+            if self.quality < 50
+              self.quality += 1
+            end
+          end
+          if self.expires_in < 6
+            if self.quality < 50
+              self.quality += 1
+            end
+          end
+        end
+      end
+    end
+    if self.name != 'Blue Distinction Plus'
+      self.expires_in -= 1
+    end
+    if self.expires_in < 0
+      if self.name != 'Blue First'
+        if self.name != 'Blue Compare'
+          if self.quality > 0
+            if self.name != 'Blue Distinction Plus'
+              self.quality -= 1
+            end
+          end
+        else
+          self.quality = self.quality - self.quality
+        end
+      else
+        if self.quality < 50
+          self.quality += 1
+        end
+      end
+    end
+  end
+end
