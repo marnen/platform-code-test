538c935545eb047446c49360c16cc66ad6eae851
Marnen Laibow-Koser <marnen@marnen.org>
2020-09-02 18:07:24 -0400

    Move all the logic into separate cases for each type.

--- award.rb
+++ award.rb
@@ -1,45 +1,44 @@
 Award = Struct.new(:name, :expires_in, :quality) do
   def update_quality!
-    if self.name != 'Blue First' && self.name != 'Blue Compare'
-      if self.quality > 0
-        if self.name != 'Blue Distinction Plus'
-          self.quality -= 1
-        end
-      end
-    else
+    case name
+    when 'Blue First'
       if self.quality < 50
         self.quality += 1
-        if self.name == 'Blue Compare'
-          if self.expires_in < 11
-            if self.quality < 50
-              self.quality += 1
-            end
+      end
+      self.expires_in -= 1
+      if self.expires_in < 0
+        if self.quality < 50
+          self.quality += 1
+        end
+      end
+    when 'Blue Compare'
+      if self.quality < 50
+        self.quality += 1
+        if self.expires_in < 11
+          if self.quality < 50
+            self.quality += 1
           end
-          if self.expires_in < 6
-            if self.quality < 50
-              self.quality += 1
-            end
+        end
+        if self.expires_in < 6
+          if self.quality < 50
+            self.quality += 1
           end
         end
       end
-    end
-    if self.name != 'Blue Distinction Plus'
       self.expires_in -= 1
-    end
-    if self.expires_in < 0
-      if self.name != 'Blue First'
-        if self.name != 'Blue Compare'
-          if self.quality > 0
-            if self.name != 'Blue Distinction Plus'
-              self.quality -= 1
-            end
-          end
-        else
-          self.quality = self.quality - self.quality
-        end
-      else
-        if self.quality < 50
-          self.quality += 1
+      if self.expires_in < 0
+        self.quality = 0
+      end
+    when 'Blue Distinction Plus'
+      # do nothing
+    else
+      if self.quality > 0
+        self.quality -= 1
+      end
+      self.expires_in -= 1
+      if self.expires_in < 0
+        if self.quality > 0
+          self.quality -= 1
         end
       end
     end
