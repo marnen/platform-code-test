0ddabc8297b6322fd8be38c547d19ad4a542c5cd
Marnen Laibow-Koser <marnen@marnen.org>
2020-09-03 18:07:44 -0400

    Extract normal award rules to module.

--- award.rb
+++ award.rb
@@ -3,35 +3,17 @@ Dir['awards/**/*.rb'].each {|file| require file }
 Award = Struct.new(:name, :expires_in, :quality) do
   def initialize(*args)
     super
-    become self.name
-  end
-
-  def update_quality!
-    modify_quality!
-    modify_expiration!
+    load_rules
   end
 
   private
 
-  def become(name)
-    mod = Awards.const_get name.delete(' ')
+  def load_rules
+    mod = begin
+      Awards.const_get self.name.delete(' ')
+    rescue NameError
+      Awards::Normal
+    end
     self.singleton_class.send :include, mod
-  rescue NameError
-    # do nothing
-  end
-
-  def modify_quality!
-    if self.quality > 0
-      self.quality -= 1
-    end
-    if self.expires_in <= 0
-      if self.quality > 0
-        self.quality -= 1
-      end
-    end
-  end
-
-  def modify_expiration!
-    self.expires_in -= 1
   end
 end
--- spec/award_spec.rb
+++ spec/award_spec.rb
@@ -3,53 +3,29 @@ require 'shared_examples/award_examples'
 require 'award'
 
 RSpec.describe Award do
+
   let(:name) { "Normal Award #{rand 100}" }
   let(:initial_expires_in) { rand(2..5) }
   let(:initial_quality) { rand(2..45) }
 
-  let(:award) { described_class.new name, initial_expires_in, initial_quality }
-
-  describe '#update_quality!' do
-    let(:update!) { -> { award.update_quality! } }
-
-    context 'conventional award' do
-      shared_examples 'quality is never negative' do
-        context do
-          let(:initial_quality) { 0 }
-
-          it 'does not lower the quality below 0' do
-            expect(update!).not_to change { award.quality }
-          end
-        end
-      end
-
-      context 'unexpired' do
-        include_examples 'decrement count of days'
-        include_examples 'quality is never negative'
-
-        it 'decrements the quality by 1' do
-          expect(update!).to change { award.quality }.by -1
-        end
-      end
-
-      context 'expired' do
-        include_context 'expired'
-
-        include_examples 'decrement count of days'
-        include_examples 'quality is never negative'
-
-        it 'decrements the quality by 2' do
-          expect(update!).to change { award.quality }.by -2
-        end
-      end
+  describe 'constructor' do
+    it 'takes parameters for name, expiration, and quality' do
+      expect(described_class.new name, initial_expires_in, initial_quality).to be_a_kind_of described_class
     end
 
-    context 'awards with special rules' do
-      subject { award }
+    context 'module inclusion' do
+      context 'normal award' do
+        it 'includes the normal behavior' do
+          expect(described_class.new name, initial_expires_in, initial_quality).to be_a_kind_of Awards::Normal
+        end
+      end
 
-      it 'includes a module for each set of special rules' do
-        ['Blue Compare', 'Blue First', 'Blue Distinction Plus'].each do |name|
-          expect(described_class.new name, 0, 0).to be_a_kind_of Awards.const_get(name.delete ' ')
+      context 'awards with special rules' do
+        it 'includes the appropriate rules module' do
+          ['Blue Compare', 'Blue First', 'Blue Distinction Plus'].each do |name|
+            mod = Awards.const_get(name.delete ' ')
+            expect(described_class.new name, initial_expires_in, initial_quality).to be_a_kind_of mod
+          end
         end
       end
     end
